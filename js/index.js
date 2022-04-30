34124e7b0b088828332564d5dbb6ce1754a0b94a
Mohamedramy390 <104658514+Mohamedramy390@users.noreply.github.com>
2022-04-30 03:34:02 +0200

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,10 @@
+function playvid(){
+    var video = document.getElementById("video");
+if(video.paused){
+     video.play();
+ } else{
+     video.pause();
+ }
+}
+
+document.getElementById("fullyear").innerHTML= new Date().getFullYear();
